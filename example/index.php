cec32547f3c9e2bb27bcfc80f355745fa795afed
Jorin Vogel <jorin.vogel@gmail.com>
2012-05-08 13:04:04 +0200

    hmmmm :D

--- example/index.php
+++ example/index.php
@@ -2,14 +2,14 @@
   <meta charset=utf-8>
   </head>
 <body>
-  <div id=gofer-container>No Homo
+  <div id=gofer-container>I am your TITLE!
 
 <img src=uploads/pic.jpg>
 <img src=uploads/pic2.png>
 awesome!
 
 <span>your favorite site:</span>
-<a href=http://youporn.com/ title=>youporn</a>
+<a href=http://www.lalalalalalalalalalalalalalalalalala.com/ title=>playboy.com</a>
 
 
 
